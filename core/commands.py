e7c284f9af1961736c82adc7b8a3876efa7adf98
Arszilla <arszilla@arszilla.com>
2020-11-11 23:09:39 +0300

    Update commands.py

--- core/commands.py
+++ core/commands.py
@@ -1,8 +1,11 @@
 from asyncio import sleep
+from os import makedirs
+from os.path import isdir, isfile
 
 import discord
 from discord.ext import commands
 
+from core import analyze, functions
 
 
 class Commands(commands.Cog):
@@ -31,6 +34,16 @@ class Commands(commands.Cog):
             value="Clears a given amount of messages from the channel the command was invoked in",
             inline=False
         )
+        embed.add_field(
+            name="analyze_linux",
+            value="Using `checksec.py`, analyze a given Linux binary",
+            inline=False
+        )
+        embed.add_field(
+            name="analyze_windows",
+            value="Using `checksec.py`, analyze a given Windows binary",
+            inline=False
+        )
 
         await context.send(embed=embed)
 
@@ -57,6 +70,87 @@ class Commands(commands.Cog):
         # delete the information after a small timeframe
         await context.send(f"Cleared {len(deleted_amount)} message(s)",
                            delete_after=1.0)
+
+    @commands.command()
+    async def analyze_linux(self, context):
+        """
+        This allows the bot to download an attachment sent with the 
+        'analyze_linux' command, then run certain commands to gather 
+        more information about the sent binary, and then feed the 
+        said information in an embed
+        """
+
+        functions.Functions().check_directory()
+
+        # Get the filename
+        filename = context.message.attachments[0].filename
+
+        filename = functions.Functions().check_file(filename)
+
+        await context.message.attachments[0].save(f"files/{filename}")
+
+        file_analysis = analyze.Analysis()
+        analysis_data = file_analysis.checksec(filename)
+
+        embed = discord.Embed(
+            title=f"File Analysis for {filename}",
+            type="rich",
+            description="**checksec**\n"
+                        f"**RELRO:** {analysis_data[0]}\n"
+                        f"**Canary:** {analysis_data[1]}\n"
+                        f"**NX:** {analysis_data[2]}\n"
+                        f"**PIE:** {analysis_data[3]}\n"
+                        f"**RPATH:** {analysis_data[4]}\n"
+                        f"**RunPath:** {analysis_data[5]}\n"
+                        f"**Symbols:** {analysis_data[6]}\n"
+                        f"**Fortify:** {analysis_data[7]}\n"
+                        f"**Fortified:** {analysis_data[8]}\n"
+                        f"**Fortibiable:** {analysis_data[9]}\n"
+                        f"**Fortify Score:** {analysis_data[10]}\n",
+            color=0xFB8B00
+        )
+
+        await context.send(embed=embed)
+
+    @commands.command()
+    async def analyze_windows(self, context):
+        """
+        This allows the bot to download an attachment sent with the 
+        'analyze_linux' command, then run certain commands to gather 
+        more information about the sent binary, and then feed the 
+        said information in an embed
+        """
+
+        functions.Functions().check_directory()
+
+        # Get the filename
+        filename = context.message.attachments[0].filename
+
+        filename = functions.Functions().check_file(filename)
+
+        await context.message.attachments[0].save(f"files/{filename}")
+
+        file_analysis = analyze.Analysis()
+        analysis_data = file_analysis.checksec(filename)
+
+        embed = discord.Embed(
+            title=f"File Analysis for {filename}",
+            type="rich",
+            description="**checksec**\n"
+                        f"**NX:** {analysis_data[0]}\n"
+                        f"**Canary:** {analysis_data[1]}\n"
+                        f"**ASLR:** {analysis_data[2]}\n"
+                        f"**Dynamic Base:** {analysis_data[3]}\n"
+                        f"**High Entropy VA:** {analysis_data[4]}\n"
+                        f"**SEH:** {analysis_data[6]}\n"
+                        f"**SafeSEH:** {analysis_data[7]}\n"
+                        f"**Force Integrity:** {analysis_data[9]}\n"
+                        f"**Control Flow Guard:** {analysis_data[8]}\n"
+                        f"**Isolation:** {analysis_data[5]}\n",
+            color=0xFB8B00
+        )
+
+        await context.send(embed=embed)
 
 
 def setup(bot):
