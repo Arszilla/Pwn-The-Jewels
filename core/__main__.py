bd7743c9714905356f76de669d46823dea444ce3
Arszilla <arszilla@arszilla.com>
2021-02-15 13:20:57 +0300

    Update __main__.py

--- core/__main__.py
+++ core/__main__.py
@@ -1,10 +1,9 @@
-import sqlite3
-
+import aiosqlite
 import discord
 from discord.ext import commands
 from discord.ext.commands import when_mentioned_or
 
-from core import constants
+from core import constants, reddit
 
 bot = commands.Bot(
     activity=discord.Game(name="Pwning The Jewels"),
@@ -15,30 +14,38 @@ bot = commands.Bot(
 )
 
 
-def connect(database):
+# Runs this function once the bot is ready.
+@bot.event
+async def on_ready():
     connection = None
+
+    # Try to connect to the database
     try:
-        connection = sqlite3.connect(database)
-        cursor = connection.cursor()
+        """
+        The database here will be used to store the items to be tracked.
+        Upon running the bot for the first time, the database and its tables will be created.
+        """
+        connection = await aiosqlite.connect(constants.Database.name)
+        cursor = await connection.cursor()
 
-        tables = list(constants.Guild.channels.keys())
-        tables.remove("reminders")
-        # # Create tables
-        for table in tables:
-            cursor.execute(f"CREATE TABLE IF NOT EXISTS {table}(id INTEGER, url TEXT)")
+        # Create Reddit related tables
+        await cursor.execute(f"CREATE TABLE IF NOT EXISTS reddit_subreddits(subreddit TEXT)")
+        await cursor.execute(f"CREATE TABLE IF NOT EXISTS reddit_posts(id TEXT, subreddit TEXT, author TEXT, title TEXT, url TEXT)")
 
-
-    except sqlite3.Error as error:
+    except aiosqlite.Error as error:
         print(error)
 
     finally:
         if connection:
-            connection.close()
+            await connection.close()
+
+    # Start the Reddit monitoring
+    reddit_monitor = reddit.Reddit(bot)
+    reddit_monitor.monitor_subreddit.start()
 
 
 if __name__ == "__main__":
     # Import commands.py in order to use commands
     bot.load_extension("core.commands")
 
-    connect(constants.Database.name)
     bot.run(constants.Bot.token)
