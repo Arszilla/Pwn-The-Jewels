b118f6b97cc8aa9526cadfcff91d730deff82446
Arszilla <arszilla@arszilla.com>
2021-02-21 17:18:18 +0300

    Update twitter.py

--- core/twitter.py
+++ core/twitter.py
@@ -70,8 +70,7 @@ class Twitter():
                         result = await boolean_cursor.fetchall()
 
                         # Grab the value for the result
-                        result = [
-                            item for state in result for item in state][0]
+                        result = [username[0] for username in result][0]
 
                         # If the value is NOT set to "True":
                         if result == 0:
@@ -104,7 +103,7 @@ class Twitter():
                         result = await boolean_cursor.fetchall()
 
                         # Grab the value for the result
-                        result = [item for state in result for item in state][0]
+                        result = [username[0] for username in result][0]
 
                         # If the value is already set to "False":
                         if result == 0:
@@ -122,127 +121,113 @@ class Twitter():
     @loop(seconds=30)
     async def monitor_tweets(self):
         try:
-            async with aiosqlite.connect(constants.Database.name) as database:
-                # Setup tweepy:
-                authentication = tweepy.OAuthHandler(constants.Twitter.consumer_key,
+            # Setup tweepy:
+            authentication = tweepy.OAuthHandler(constants.Twitter.consumer_key,
                                                     constants.Twitter.consumer_secret)
 
-                authentication.set_access_token(constants.Twitter.access_token,
-                                                constants.Twitter.access_token_secret)
+            authentication.set_access_token(constants.Twitter.access_token,
+                                            constants.Twitter.access_token_secret)
 
-                twitter_api = tweepy.API(authentication)
+            twitter_api = tweepy.API(authentication)
 
-                # Define the channel that'll be used to send the embeds:
-                channel = self.bot.get_channel(constants.Channels.twitter_rss)
+            # Define the channel that'll be used to send the embeds:
+            channel = self.bot.get_channel(constants.Channels.twitter_rss)
 
-                async with database.execute("SELECT * FROM twitter_usernames") as user_cursor:
-                    user_list = await user_cursor.fetchall()
+            async with aiosqlite.connect(constants.Database.name) as database:
+                async with database.execute("SELECT * FROM twitter_usernames") as twitter_cursor:
+                    twitter_list = await twitter_cursor.fetchall()
 
-                    # Turn the list of tuples into a list:
-                    user_list = [user[0] for user in user_list]
+                for username, rt_status in twitter_list:
+                    # If retweets are disabled:
+                    if rt_status == 0:
+                        user_feed = twitter_api.user_timeline(screen_name=username,
+                                                              include_rts=False,
+                                                              exclude_replies=True,
+                                                              tweet_mode="extended",
+                                                              count=1)
 
-                    for username in user_list:
-                        # Check the retweet status of the given user:
-                        async with database.execute("SELECT retweets FROM twitter_usernames WHERE username=?",
-                                                    (username,)) as rt_cursor:
-                            rt_status = await rt_cursor.fetchall()
+                    # If retweets are enabled:
+                    elif rt_status == 1:
+                        user_feed = twitter_api.user_timeline(screen_name=username,
+                                                              include_rts=True,
+                                                              exclude_replies=False,
+                                                              tweet_mode="extended",
+                                                              count=1)
 
-                            # Turn the list of tuples into a list:
-                            rt_status = [status[0] for status in rt_status][0]
+                    tweet = user_feed[0]
 
-                            # If retweets are disabled:
-                            if rt_status == 0:
-                                user_feed = twitter_api.user_timeline(screen_name=username,
-                                                                    include_rts=False,
-                                                                    exclude_replies=True,
-                                                                    tweet_mode="extended",
-                                                                    count=1)
+                    # Check if the given tweet exists in the database:
+                    async with database.execute("SELECT id FROM twitter_tweets WHERE id=?",
+                                                (tweet.id,)) as id_cursor:
+                        result = await id_cursor.fetchall()
 
-                            # If retweets are enabled:
-                            elif rt_status == 1:
-                                user_feed = twitter_api.user_timeline(screen_name=username,
-                                                                    include_rts=True,
-                                                                    exclude_replies=False,
-                                                                    tweet_mode="extended",
-                                                                    count=1)
+                        # If the tweet does NOT exist in the database:
+                        if len(result) == 0:
+                            # The line below has been commented out because
+                            # stuff like Spotify etc. use "https://t.co/" links.
+                            # As a result, this leaves some embeds looking weird.
+                            # For now, "https://t.co/" links will stay in embeds,
+                            # until a proper solution can be found.
 
-                            tweet = user_feed[0]
+                            # Remove any "https://t.co/" URLs in the tweet:
+                            # tweet_text = sub(r"https://t.co/\w+", "", tweet.full_text)
 
-                        # Check if the given tweet exists in the database:
-                        async with database.execute("SELECT id FROM twitter_tweets WHERE id=?",
-                                                    (tweet.id,)) as id_cursor:
-                            result = await id_cursor.fetchall()
+                            await database.execute("INSERT INTO twitter_tweets VALUES (?, ?, ?, ?)",
+                                                   (tweet.id,
+                                                    username,
+                                                    tweet.full_text,
+                                                    f"https://twitter.com/{username}/status/{tweet.id}"))
+                            await database.commit()
 
-                            # If the tweet does NOT exist in the database:
-                            if len(result) == 0:
-                                # The line below has been commented out because 
-                                # stuff like Spotify etc. use "https://t.co/" links. 
-                                # As a result, this leaves some embeds looking weird.
-                                # For now, "https://t.co/" links will stay in embeds, 
-                                # until a proper solution can be found.
-
-                                # Remove any "https://t.co/" URLs in the tweet:
-                                # tweet_text = sub(r"https://t.co/\w+", "", tweet.full_text)
-
-                                await database.execute("INSERT INTO twitter_tweets VALUES (?, ?, ?, ?)",
-                                                       (tweet.id,
-                                                        username,
-                                                        tweet.full_text,
-                                                        f"https://twitter.com/{username}/status/{tweet.id}"))
-                                await database.commit()
-
-                                # Check if the tweet has "RT" at the beginning of the string.
-                                # This is done in order to remove the retweet indicator later on.
-                                if tweet.full_text.split()[0] == "RT":
-                                    # Embed the retweet
-                                    embed = discord.Embed(
-                                        title=f"{username} just retweeted",
-                                        url=f"https://twitter.com/{username}/status/{tweet.id}",
-                                        type="rich",
-                                        description=tweet.full_text.split(" ", 1)[1],
-                                        color=0x1DA1F2
-                                    )
-
-                                else:
-                                    # Embed the tweet
-                                    embed = discord.Embed(
-                                        title=f"{username} just tweeted",
-                                        url=f"https://twitter.com/{username}/status/{tweet.id}",
-                                        type="rich",
-                                        description=tweet.full_text,
-                                        color=0x1DA1F2
-                                    )
-
-                                # Embed the author of the post.
-                                embed.set_author(
-                                    name=f"@{username}",
-                                    url=f"https://twitter.com/{username}",
-                                    icon_url=tweet.user.profile_image_url
+                            # Check if the tweet has "RT" at the beginning of the string.
+                            # This is done in order to remove the retweet indicator later on.
+                            if tweet.full_text.split()[0] == "RT":
+                                # Embed the retweet
+                                embed = discord.Embed(
+                                    title=f"{username} just retweeted",
+                                    url=f"https://twitter.com/{username}/status/{tweet.id}",
+                                    type="rich",
+                                    description=tweet.full_text.split(" ", 1)[1],
+                                    color=0x1DA1F2
                                 )
 
-                                # If there are any images in the tweet, grab the first image:
-                                if "media" in tweet.entities:
-                                    embed.set_image(
-                                        url=tweet.extended_entities["media"][0]["media_url"]
-                                    )
-
-                                # Embed the footer
-                                embed.set_footer(
-                                    text="Pwn The Jewels"
-                                )
-
-                                # Embed the time
-                                embed.timestamp = datetime.utcnow()
-
-                                # Send the Discord embed to the Twitter channel.
-                                await channel.send(embed=embed)
-
-                            # If the tweet exists in the database, move on to the next user.
                             else:
-                                continue
-        
-        # When the database is first initalized, because there are no usernames,
-        # Line 143 throws IndexError. This is meant to catch it and hope till the next loop,
-        # that a username is submitted to the database.
+                                # Embed the tweet
+                                embed = discord.Embed(
+                                    title=f"{username} just tweeted",
+                                    url=f"https://twitter.com/{username}/status/{tweet.id}",
+                                    type="rich",
+                                    description=tweet.full_text,
+                                    color=0x1DA1F2
+                                )
+
+                            # Embed the author of the post.
+                            embed.set_author(
+                                name=f"@{username}",
+                                url=f"https://twitter.com/{username}",
+                                icon_url=tweet.user.profile_image_url
+                            )
+
+                            # If there are any images in the tweet, grab the first image:
+                            if "media" in tweet.entities:
+                                embed.set_image(
+                                    url=tweet.extended_entities["media"][0]["media_url"]
+                                )
+
+                            # Embed the footer
+                            embed.set_footer(
+                                text="Pwn The Jewels"
+                            )
+
+                            # Embed the time
+                            embed.timestamp = datetime.utcnow()
+
+                            # Send the Discord embed to the Twitter channel.
+                            await channel.send(embed=embed)
+
+                        # If the tweet exists in the database, move on to the next user.
+                        else:
+                            continue
+
         except IndexError:
             pass
